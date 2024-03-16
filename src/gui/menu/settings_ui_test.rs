ff6e5b9da852c6bca86eb504c4d8ac928d74cd63
Zanderwohl <zandy@zanderlowry.com>
2024-03-16 16:37:38 -0500

    UI Test back button

--- src/gui/menu/settings_ui_test.rs
+++ src/gui/menu/settings_ui_test.rs
@@ -1,6 +1,8 @@
-use bevy::prelude::{Commands, Component, Res};
+use bevy::prelude::{AlignItems, BuildChildren, ButtonBundle, Color, Commands, Component, default, FlexDirection, NodeBundle, Res, Style, TextBundle, Val};
 use crate::gui::common;
 use crate::gui::common::{text, Volume};
+use crate::gui::common::color::NORMAL_BUTTON;
+use crate::gui::menu::main::{MenuButtonAction, SelectedOption};
 
 #[derive(Component)]
 pub(crate) struct OnUITestScreen;
@@ -11,5 +13,46 @@ pub fn ui_test_menu_setup(mut commands: Commands) {
 
     let base_screen = common::base_screen(&mut commands);
     commands.entity(base_screen)
-        .insert(OnUITestScreen);
+        .insert(OnUITestScreen)
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: Color::CRIMSON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                align_items: AlignItems::Center,
+                                ..default()
+                            },
+                            background_color: Color::CRIMSON.into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle::from_section(
+                                "UI TEST",
+                                button_text_style.clone(),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: button_style,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MenuButtonAction::BackToSettings,
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle::from_section("Back", button_text_style));
+                        });
+                });
+        });
 }
