b8e3b368660ff5e45d4cb410bc368b0c13a2eef5
zandy <zandy@zandy.xyz>
2025-03-02 03:16:27 -0600

    Time slider

--- src/gui/planetarium/mod.rs
+++ src/gui/planetarium/mod.rs
@@ -1,10 +1,10 @@
 use bevy::app::{App, Update};
-use bevy::prelude::{in_state, Commands, IntoSystemSetConfigs, NextState, Plugin, ResMut, Startup, SystemSet};
+use bevy::prelude::{in_state, Commands, IntoSystemSetConfigs, NextState, Plugin, Res, ResMut, Startup, SystemSet, Time};
 use bevy::prelude::IntoSystemConfigs;
 use bevy_egui::{egui, EguiContexts};
 use crate::gui::app::AppState;
 use crate::gui::menu::{MenuState, UiState};
-use crate::gui::planetarium::time::Time;
+use crate::gui::planetarium::time::SimTime;
 use crate::gui::settings::Settings;
 
 pub mod time;
@@ -18,12 +18,12 @@ pub struct Planetarium;
 impl Plugin for Planetarium {
     fn build(&self, app: &mut App) {
         app
-            .init_resource::<Time>()
+            .init_resource::<SimTime>()
             .configure_sets(Update, (
                 PlanetariumUISet.run_if(in_state(AppState::Planetarium)),
             ))
             .add_systems(Update, (
-                (planetarium_ui,).in_set(PlanetariumUISet),
+                (planetarium_ui, advance_time).in_set(PlanetariumUISet),
             ))
         ;
     }
@@ -35,7 +35,7 @@ fn planetarium_ui(
     mut ui_state: ResMut<UiState>,
     mut next_app_state: ResMut<NextState<AppState>>,
     mut next_menu_state: ResMut<NextState<MenuState>>,
-    mut time: ResMut<Time>,
+    mut time: ResMut<SimTime>,
 ) {
     let ctx = contexts.ctx_mut();
 
@@ -52,6 +52,34 @@ fn planetarium_ui(
                 ui.button("Save");
             });
             ui.separator();
-            ui.label(format!("Time: {:.1}s", time.time))
+            ui.horizontal(|ui| {
+               if time.playing {
+                   if ui.button("Pause").clicked() {
+                       time.playing = false;
+                   }
+               } else {
+                   if ui.button("Play").clicked() {
+                       time.playing = true;
+                   }
+               }
+                ui.label(format!("Time: {:.1}s", time.time))
+            });
+            ui.horizontal(|ui| {
+                ui.label("Simulation speed");
+                ui.add(egui::DragValue::new(&mut time.gui_speed)
+                    .speed(0.1)
+                    .range(-100.0..=100.0)
+                    .fixed_decimals(1)
+                );
+            });
+
+            ui.separator();
     });
+}
+
+fn advance_time(mut sim_time: ResMut<SimTime>, time: Res<Time>) {
+    if sim_time.playing {
+        sim_time.previous_time = sim_time.time;
+        sim_time.time += sim_time.gui_speed * time.delta_secs_f64();
+    }
 }
--- src/gui/planetarium/time.rs
+++ src/gui/planetarium/time.rs
@@ -1,16 +1,22 @@
 use bevy::prelude::Resource;
 
 #[derive(Resource)]
-pub struct Time {
+pub struct SimTime {
     pub time: f64,
+    pub previous_time: f64,
     pub step: f64,
+    pub gui_speed: f64,
+    pub playing: bool,
 }
 
-impl Default for Time {
+impl Default for SimTime {
     fn default() -> Self {
         Self {
             time: 0.0,
+            previous_time: 0.0,
             step: 0.1,
+            gui_speed: 1.0,
+            playing: false,
         }
     }
 }
